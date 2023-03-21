56e2c3d7ee6e48b7c5559dce2cded08e45036c1b
hamza <hamzamadani217@gmail.com>
2023-03-21 20:42:54 +0000

    first push

---
+++ src/main/java/akaza/gdt/com/GestionDesTerainApplication.java
@@ -0,0 +1,13 @@
+package akaza.gdt.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesTerainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesTerainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akaza/gdt/com/GestionDesTerainApplicationTests.java
@@ -0,0 +1,13 @@
+package akaza.gdt.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesTerainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
